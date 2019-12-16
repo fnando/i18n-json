37285119aefd58cb1d7fa84956ff0ec7e94c6c8a
Nando Vieira <me@fnando.com>
2019-12-16 00:55:04 -0800

    Bump up version.

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# Changelog
+
+## [Unreleased]
+
+Nothing
+
+## [0.0.1] 2019-12-16
+
+- Initial release
--- lib/i18n-json/version.rb
+++ lib/i18n-json/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module I18nJSON
-  VERSION = "0.0.0"
+  VERSION = "0.0.1"
 end
